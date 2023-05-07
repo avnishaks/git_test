f329ceff41f7b3263634116c63208f655fb4ca74
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-07 08:00:25 +0530

    React SetUp , React DOM , Project Structure

--- react-js-intro/src/App.js
+++ react-js-intro/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 
+let app='App'
+let app_header='App-header'
+
 function App() {
+  console.log('constructing App Application')
   return (
-    <div className="App">
-      <header className="App-header">
-        Avnish Kumar Singh
+    <div className={app}>
+      <header className={app_header}>
+        App Application
+        <Demo></Demo>
+      </header>
+    </div>
+  );
+}
+
+function Demo() {
+  console.log('constructing Demo Application');
+  return (
+    <div className={app}>
+      <header className={app_header}>
+         Demo Application
       </header>
     </div>
   );
--- react-js-intro/src/index.js
+++ react-js-intro/src/index.js
@@ -6,8 +6,12 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //<App/> // -> by removing the StrictMode wrapper in React duplicate element is removed
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
 
